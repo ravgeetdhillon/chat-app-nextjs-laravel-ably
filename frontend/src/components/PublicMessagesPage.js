34f3f2a202cf3a4dd190a205fd1f2c9ffa7a530d
ravgeetdhillon <ravgeetdhillon@gmail.com>
2022-02-21 19:47:49 +0530

    refactor: migrated code to react.js from next.js

--- frontend/src/components/PublicMessagesPage.js
+++ frontend/src/components/PublicMessagesPage.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Axios from 'axios';
-import Messagebox from '../components/Messagebox';
+import Messagebox from './Messagebox';
 
 export default class PublicMessagesPage extends React.Component {
   constructor() {
     super();
     this.state = { user: '', message: '', messages: [] };
-    Axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+    Axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
   }
 
   componentDidMount() {
-    Echo.channel('public.room')
+    window.Echo.channel('public.room')
       .subscribed(() => {
         console.log('You are subscribed');
       })
       .listen('.message.new', (data) => {
-        data.isOwner = data.user === this.state.user;
         this.setState({ messages: [...this.state.messages, data], message: '' });
       });
   }
@@ -45,39 +44,39 @@ export default class PublicMessagesPage extends React.Component {
 
   render() {
     return (
-      <div className="container w-100 vh-100 bg-dark text-white">
-        <div className="d-flex h-100 flex-column">
-          <div className="p-3">
-            <h1 className="h5 mb-1">Public Space</h1>
-            <p className="text-muted">Post your random thoughts for the world to see</p>
+      <div className="...">
+        <div className="...">
+          <div className="...">
+            <h1 className="...">Public Space</h1>
+            <p className="...">Post your random thoughts for the world to see</p>
           </div>
 
-          <div className="d-flex flex-column justify-content-end pt-3 h-100 overflow-auto">
+          <div className="...">
             {this.state.messages.map((message) => (
               <Messagebox key={message.id} message={message} />
             ))}
           </div>
 
-          <div className="py-2">
-            <form className="overflow-hidden" onSubmit={(e) => this.handleSendMessage(e)}>
+          <div className="...">
+            <form className="..." onSubmit={(e) => this.handleSendMessage(e)}>
               <input
                 type="text"
-                className="form-control form-control-sm text-white bg-dark rounded-0 border-dark ps-0"
+                className="..."
                 placeholder="Set your username"
                 value={this.state.user}
                 onChange={(e) => this.setState({ user: e.target.value })}
                 required
               />
-              <div className="d-flex align-items-center overflow-hidden">
+              <div className="...">
                 <input
                   type="text"
-                  className="form-control text-white bg-dark rounded-0 border-dark ps-0"
+                  className="..."
                   placeholder="Type your message..."
                   value={this.state.message}
                   onChange={(e) => this.setState({ message: e.target.value })}
                   required
                 />
-                <button className="btn btn-primary" onClick={(e) => this.handleSendMessage(e)}>
+                <button className="..." onClick={(e) => this.handleSendMessage(e)}>
                   Send
                 </button>
               </div>
